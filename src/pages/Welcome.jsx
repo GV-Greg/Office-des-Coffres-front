b21254563344b3fa344dd21768f2941ab1b95fff
Greg GV <gregvandenbergh@gmail.com>
2021-11-27 13:53:13 +0100

    Updates

--- src/pages/Welcome.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <div className="overflow-y-auto flex flex-col flex-grow">
-            <h2>Bienvenue !</h2>
-            {/*<div>Vous êtes entré dans l'Office des coffres.</div>*/}
-            {/*<p></p>*/}
-        </div>
-    );
-}

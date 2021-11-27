5f4a30b165b60d4560c15d179667f0d9410dbf21
Greg GV <gregvandenbergh@gmail.com>
2021-11-27 22:25:47 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content:['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    safelist: [
+      'text-gray-100',
+      'ring-offset-gray-100',
+      'ring-gray-400',
+      'bg-gray-500',
+      'bg-gray-600',
+      'text-yellow-100',
+      'ring-offset-yellow-100',
+      'ring-yellow-400',
+      'bg-yellow-500',
+      'bg-yellow-600',
+      'text-salmon-100',
+      'ring-offset-salmon-100',
+      'ring-salmon-400',
+      'bg-salmon-500',
+      'bg-salmon-600',
+      'text-teal-100',
+      'ring-offset-teal-100',
+      'ring-teal-400',
+      'bg-teal-500',
+      'bg-teal-600',
+      'text-blueGray-100',
+      'ring-offset-blueGray-100',
+      'ring-blueGray-400',
+      'bg-blueGray-500',
+      'bg-blueGray-600',
+
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     container: {

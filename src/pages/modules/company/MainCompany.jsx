9fc26de146e529ba427a88b673ad50eb6e94509f
Greg GV <gregvandenbergh@gmail.com>
2022-01-04 12:39:02 +0100

    Commit MainCompany

--- src/pages/modules/company/MainCompany.jsx
+++ src/pages/modules/company/MainCompany.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-export default function Company() {
-    return (
-        <div className="overflow-y-auto flex flex-col flex-grow">
-            <h2>Page Compagnie Vit'Art</h2>
-        </div>
-    );
-}
-=======
 const Company = () => {
     return (
         <div className="overflow-y-auto flex flex-col flex-grow">
@@ -16,4 +7,3 @@ const Company = () => {
 }
 
 export default Company;
->>>>>>> develop

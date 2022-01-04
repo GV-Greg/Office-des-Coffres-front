07d79ad31f53284e5ee19148ddc7804eb697e1ed
Greg GV <gregvandenbergh@gmail.com>
2022-01-04 12:53:53 +0100

    Clean tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,6 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-<<<<<<< HEAD
-  purge: {
-    content:['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    safelist: [
-      'text-gray-100',
-      'ring-offset-gray-100',
-      'ring-gray-400',
-      'bg-gray-500',
-      'bg-gray-600',
-      'text-yellow-100',
-      'ring-offset-yellow-100',
-      'ring-yellow-400',
-      'bg-yellow-500',
-      'bg-yellow-600',
-      'text-salmon-100',
-      'ring-offset-salmon-100',
-      'ring-salmon-400',
-      'bg-salmon-500',
-      'bg-salmon-600',
-      'text-teal-100',
-      'ring-offset-teal-100',
-      'ring-teal-400',
-      'bg-teal-500',
-      'bg-teal-600',
-      'text-blueGray-100',
-      'ring-offset-blueGray-100',
-      'ring-blueGray-400',
-      'bg-blueGray-500',
-      'bg-blueGray-600',
-
-    ]
-  },
-  darkMode: false, // or 'media' or 'class'
-=======
   content: [
       "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -44,7 +10,6 @@ module.exports = {
     { pattern: /ring-(gray|yellow|salmon|teal|slate|blue)-(100|200|300|400|500|600|700|800|900)/, },
     { pattern: /bg-(gray|yellow|salmon|teal|slate|blue)-(100|200|300|400|500|600|700|800|900)/, },
   ],
->>>>>>> develop
   theme: {
     container: {
       center: true,
@@ -54,11 +19,7 @@ module.exports = {
       current: 'currentColor',
       black: colors.black,
       white: colors.white,
-<<<<<<< HEAD
-      blueGray: colors.blueGray,
-=======
       slate: colors.slate,
->>>>>>> develop
       gray: colors.gray,
       red: colors.red,
       orange: colors.orange,
@@ -72,11 +33,6 @@ module.exports = {
       pink: colors.fuchsia,
       salmon: colors.rose,
     },
-<<<<<<< HEAD
-    extend: {},
-  },
-  variants: {
-=======
     fontSize: {
       'xs': '.75rem',
       'sm': '.875rem',
@@ -105,7 +61,6 @@ module.exports = {
       '225': '225deg',
       '270': '270deg',
     },
->>>>>>> develop
     extend: {},
   },
   plugins: [],

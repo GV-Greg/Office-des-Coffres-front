cc7d2766df0a81686a57228a9b5d66799d3b919e
Greg GV <gregvandenbergh@gmail.com>
2022-01-04 12:40:44 +0100

    Delete old folders

--- src/pages/modules/anim/MainAnimation.jsx
+++
@@ -1,7 +0,0 @@
-export default function Anim() {
-    return (
-        <div className="overflow-y-auto flex flex-col flex-grow">
-            <h2>Page Animation</h2>
-        </div>
-    );
-}
--- src/pages/modules/secu/MainSecurite.jsx
+++
@@ -1,7 +0,0 @@
-export default function Secu() {
-    return (
-        <div className="overflow-y-auto flex flex-col flex-grow">
-            <h2>Page Sécurité</h2>
-        </div>
-    );
-}

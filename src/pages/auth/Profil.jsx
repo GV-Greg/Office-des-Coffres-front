c4891aef4fb3ec757404aa79e3da4b6ba6890932
Greg GV <gregvandenbergh@gmail.com>
2022-01-04 12:36:46 +0100

    commit Profil

--- src/pages/auth/Profil.jsx
+++ src/pages/auth/Profil.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-export default function Profil() {
-=======
 const Profile = () => {
->>>>>>> develop
     return (
         <div className="overflow-y-auto flex flex-col flex-grow">
             <h2>Profil</h2>
         </div>
     );
-<<<<<<< HEAD
-}
-=======
 }
 
 export default Profile;
->>>>>>> develop
